cad4f330de3d5d32171a604d8b478bfcd29536de
nobrenoivas7-a11y <nobrenoivas7@gmail.com>
2026-06-27 18:59:10 -0300

    Update contrato.py

--- backend/models/contrato.py
+++ backend/models/contrato.py
@@ -27,4 +27,40 @@ class Contrato(db.Model):
         return max(0.0, self.valor_total - self.valor_pago)
 
     def atualizar_status(self):
-        if
+        if self.status == 'ativo' and self.data_devolucao < date.today():
+            self.status = 'atrasado'
+
+    def __repr__(self):
+        return f'<Contrato #{self.id:04d}>'
+
+
+class ContratoItem(db.Model):
+    __tablename__ = 'contrato_itens'
+
+    id = db.Column(db.Integer, primary_key=True)
+    contrato_id = db.Column(db.Integer, db.ForeignKey('contratos.id'), nullable=False)
+    peca_id = db.Column(db.Integer, db.ForeignKey('pecas.id'), nullable=False)
+    preco_cobrado = db.Column(db.Float, default=0.0)
+
+
+class Pagamento(db.Model):
+    __tablename__ = 'pagamentos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    contrato_id = db.Column(db.Integer, db.ForeignKey('contratos.id'), nullable=False)
+    valor = db.Column(db.Float, nullable=False)
+    tipo = db.Column(db.String(20), default='sinal')
+    forma = db.Column(db.String(20), default='pix')
+    observacao = db.Column(db.String(200))
+    data = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Despesa(db.Model):
+    __tablename__ = 'despesas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    descricao = db.Column(db.String(200), nullable=False)
+    valor = db.Column(db.Float, nullable=False)
+    categoria = db.Column(db.String(50))
+    data = db.Column(db.Date, nullable=False)
+    criado_em = db.Column(db.DateTime, default=datetime.utcnow)
